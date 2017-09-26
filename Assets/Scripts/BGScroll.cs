951366a46554265d269c865240377c1cc3b7ca22
BREUILLÉ Édouard <Oxedora@users.noreply.github.com>
2017-09-26 23:53:42 +0200

    Added enemies and build v2

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tilesizeZ;
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tilesizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
